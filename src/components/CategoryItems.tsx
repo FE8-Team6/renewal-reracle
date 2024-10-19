72ff148616ccb5753c9e17dfabbe432ca88b1ede
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-10-19 15:25:02 +0900

    refactor: getCategoryItmes 함수 실행되도록 코드 수정

--- src/components/CategoryItems.tsx
+++ src/components/CategoryItems.tsx
@@ -8,25 +8,26 @@ const CategoryItems = () => {
   const { categoryId } = useParams();
   const [categoryItems, setCategoryItems] = useState([]);
 
-  useEffect(() => {
-    const getCategoryItems = async () => {
-      try {
-        if (!categoryId) {
-          return <>아이템이 없습니다.</>;
-        }
-        const categoryRef = doc(db, "WasteCategories", categoryId);
-        const categorySnap = await getDoc(categoryRef);
-
-        if (categorySnap.exists()) {
-          setCategoryItems(categorySnap.data().items);
-        } else {
-          console.log("No such document!");
-        }
-        getCategoryItems();
-      } catch (error) {
-        console.error("Error getting document:", error);
+  const getCategoryItems = async () => {
+    try {
+      if (!categoryId) {
+        return <>아이템이 없습니다.</>;
       }
-    };
+      const categoryRef = doc(db, "WasteCategories", categoryId);
+      const categorySnap = await getDoc(categoryRef);
+
+      if (categorySnap.exists()) {
+        setCategoryItems(categorySnap.data().items);
+      } else {
+        console.log("카테고리가 존재하지 않습니다.");
+      }
+    } catch (error) {
+      console.error("Error getting document:", error);
+    }
+  };
+
+  useEffect(() => {
+    getCategoryItems();
   }, [categoryId]);
 
   return (
