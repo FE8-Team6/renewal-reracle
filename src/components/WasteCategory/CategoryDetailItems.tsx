e00abf05f32d9eca1ac1012cc44ddaa7a94b968f
wook <yho7955@naver.com>
2024-10-21 10:30:25 +0900

    feat: categoryDetailItems 추가

--- src/components/WasteCategory/CategoryDetailItems.tsx
+++ src/components/WasteCategory/CategoryDetailItems.tsx
@@ -1,23 +1,31 @@
 import { db } from "@/firebase";
 import { doc, getDoc } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+type ItemsDetails = {
+  id: string;
+  name: string;
+  imageURL: string;
+  description: string;
+};
 
 const CategoryDetailItems = () => {
   const { categoryId, itemId } = useParams();
-  const [itemsDetails, setItemsDetails] = useState();
+  const [itemsDetails, setItemsDetails] = useState<ItemsDetails>();
 
   const getDetailItems = async () => {
+    if (!categoryId || !itemId) {
+      return <>아이템이 없습니다.</>;
+    }
+
     try {
-      if (!categoryId || !itemId) {
-        return <>아이템이 없습니다.</>;
-      }
       const categoryRef = doc(db, "WasteCategories", categoryId);
       const categorySnap = await getDoc(categoryRef);
 
       if (categorySnap.exists()) {
         const itmes = categorySnap.data().items;
-        const item = itmes.find((item) => item.id === itemId);
+        const item = itmes.find((item: ItemsDetails) => item.id === itemId);
         setItemsDetails(item);
       } else {
         console.log("카테고리가 존재하지 않습니다.");
@@ -32,14 +40,23 @@ const CategoryDetailItems = () => {
   }, [categoryId, itemId]);
 
   return (
-    <div>
-      <h1>{itemsDetails?.name}</h1>
-      <img
-        src={itemsDetails?.imageURL}
-        alt={itemsDetails?.name}
-        className="w-[10rem] h-[10rem]"
-      />
-      <p>{itemsDetails?.description}</p>
+    <div className="fixed w-full h-auto top-20">
+      <h3 className="text-xl font-bold text-purple ml-[6vh]">
+        {itemsDetails?.name}
+      </h3>
+      {itemsDetails?.imageURL && (
+        <div className="w-[20rem] h-[12rem] bg-purpleLight rounded-lg flex justify-center items-center my-4 mx-auto">
+          <img
+            src={itemsDetails.imageURL}
+            alt={itemsDetails.name}
+            className="max-w-[40%] h-auto object-contain"
+          />
+        </div>
+      )}
+      <h3 className="text-xl font-bold text-purple ml-[6vh]">배출방법</h3>
+      <p className="w-[20rem] mx-auto my-4 font-semibold ml-[6vh]">
+        {itemsDetails?.description}
+      </p>
     </div>
   );
 };
