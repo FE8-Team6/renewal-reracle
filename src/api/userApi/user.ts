d504a4be3e3e466b9a93b37df9f107182a10b928
wook <yho7955@naver.com>
2024-10-22 11:39:47 +0900

    refactor: mypage layout 수정

--- src/api/userApi/user.ts
+++ src/api/userApi/user.ts
@@ -1,6 +1,11 @@
 import { db } from "@/firebase";
 import { doc, getDoc } from "firebase/firestore";
 
+/**
+ * 사용자의 프로필 정보를 가져오는 함수
+ * @param uid
+ * @returns
+ */
 const getUserProfile = async (uid: string) => {
   const userDoc = await getDoc(doc(db, "users", uid));
   if (userDoc.exists()) {
--- src/components/MyPage/MyPage.tsx
+++ src/components/MyPage/MyPage.tsx
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { PurpleButton, WhiteButton } from "../Buttons";
-import UserInfo from "./UserInfo";
 import { Layout } from "@/components/layout/Layout";
 import { useSearchStore } from "@/lib/store/useSearchStore";
 import LoginToSignUpTitle from "../LoginToSignUpTitle";
-
-const getUserData = () => {
-  const data = localStorage.getItem("userData");
-  return data ? JSON.parse(data) : null;
-};
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import {
+  MdAlternateEmail,
+  MdOutlineDriveFileRenameOutline,
+} from "react-icons/md";
 
 const MyPage = () => {
-  const [user, setUser] = useState({
+  const [user, setUser] = useState<{ nickname: string; email: string }>({
     nickname: "",
     email: "",
   });
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const storedUser = getUserData();
+    const userData = localStorage.getItem("userData");
+    const storedUser = userData ? JSON.parse(userData) : null;
     if (storedUser) {
       setUser(storedUser);
     }
   }, []);
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUser((u) => ({ ...u, nickname: event.target!.value }));
+    setUser((user) => ({ ...user, nickname: event.target!.value }));
   };
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUser((u) => ({ ...u, email: event.target!.value }));
+    setUser((user) => ({ ...user, email: event.target!.value }));
   };
 
   const handleClick = (
@@ -41,45 +42,57 @@ const MyPage = () => {
   };
 
   const searchHistory = useSearchStore((state) => state.searchHistory);
-  const navigate = useNavigate();
 
   const handleNavClick = (categoryId: string, itemId: string) => {
     navigate(`/${categoryId}/${itemId}`);
   };
 
-  const handleGoToMyQuestions = () => {
-    navigate("/myquestion");
-  };
-
   return (
     <Layout>
-      <section className="w-full h-auto bg-white relative flex flex-col items-center gap-[2vh] overflow-y-auto">
+      <section className="w-full h-full bg-white relative flex flex-col items-center gap-[2vh] overflow-y-auto">
         <LoginToSignUpTitle title="프로필" />
         <div className="mt-4">
-          <UserInfo
-            label="닉네임"
-            type="text"
-            value={user.nickname}
-            onChange={handleNameChange}
-          >
-            아이디
-          </UserInfo>
-          <UserInfo
-            label="이메일"
-            type="email"
-            value={user.email}
-            onChange={handleEmailChange}
-          >
-            이메일
-          </UserInfo>
+          <div className="w-full h-[5rem] relative">
+            <label className="text-lg font-bold text-purple">닉네임</label>
+            <MdOutlineDriveFileRenameOutline className="absolute text-xl left-3 top-10 text-purple" />
+            <Input
+              type="text"
+              value={user.nickname}
+              onChange={handleNameChange}
+              placeholder="닉네임을 입력해주세요"
+              className="w-full h-[2.5rem] text-xl bg-transparent focus:outline-none focus:shadow-none pl-10"
+            />
+          </div>
+          <div className="w-full h-[5rem] relative">
+            <label className="text-lg font-bold text-purple">이메일</label>
+            <MdAlternateEmail className="absolute text-xl left-3 top-10 text-purple" />
+            <Input
+              type="email"
+              value={user.email}
+              onChange={handleEmailChange}
+              placeholder="이메일을 입력해주세요"
+              className="w-full h-[2.5rem] text-xl bg-transparent focus:outline-none focus:shadow-none pl-10"
+            />
+          </div>
         </div>
-        <PurpleButton onClick={handleClick}>회원정보 수정</PurpleButton>
-        <WhiteButton onClick={handleGoToMyQuestions}>
-          나의 R지식in 보러가기
-        </WhiteButton>
-        <div className="w-[46vh]">
-          <div className="w-[46vh] h-[1px] mt-[2vh] mb-[0.1vh] bg-purple"></div>
-          <span className="text-[2vh] font-bold text-purple">
+        <Button
+          variant="default"
+          type="submit"
+          size="default"
+          onClick={handleClick}
+        >
+          회원정보 수정
+        </Button>
+        <Button
+          variant="link"
+          size="sm"
+          onClick={() => navigate("/myquestion")}
+        >
+          <span> 나의 R지식in 보러가기</span>
+        </Button>
+        <div className="w-full">
+          <div className="w-full h-[1px] mt-4 mb-1 bg-purple" />
+          <span className="ml-2 text-lg font-bold text-purple">
             나의 최근 재활용품 검색 리스트
           </span>
           <ul className="w-[46vh] h-[4vh] flex flex-wrap py-[2vh] gap-[1.5vh]">
