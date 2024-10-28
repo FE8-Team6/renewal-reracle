98b34ded6fa0b32c0a7024d73ec322fe8aee1192
wook <yho7955@naver.com>
2024-10-28 11:37:54 +0900

    feat: 분리 수거 용품 검색 기능 구현

--- src/lib/common/CategorySearchBar.tsx
+++ src/lib/common/CategorySearchBar.tsx
@@ -1,16 +1,37 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AlertCircle, Search, X } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { useNavigate } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase";
 
 interface SearchBarProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: string;
 }
 
+type SearchResults = {
+  categoryId: string;
+  id: string;
+  name: string;
+}[];
+
+type Category = {
+  id: string;
+  items: {
+    name: string;
+    id: string;
+    imageURL: string;
+    description: string;
+  }[];
+};
+
 const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
   ({ error, className, ...props }, ref) => {
     const [value, setValue] = useState("");
+    const [searchResults, setSearchResults] = useState<SearchResults>([]);
+    const navigate = useNavigate();
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       setValue(event.target.value);
@@ -21,6 +42,7 @@ const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
 
     const handleClear = () => {
       setValue("");
+      setSearchResults([]);
       if (props.onChange) {
         props.onChange({
           target: { value: "" },
@@ -28,11 +50,45 @@ const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
       }
     };
 
+    const handleSearch = async () => {
+      if (value.trim() === "") {
+        setSearchResults([]);
+        return;
+      }
+
+      const categoriesCollectionRef = collection(db, "WasteCategories");
+      const categoriesSnap = await getDocs(categoriesCollectionRef);
+      const categoriesData = categoriesSnap.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Category[];
+
+      const results = categoriesData.flatMap((category) =>
+        category.items
+          .filter((item) =>
+            item.name?.toLowerCase().includes(value.toLowerCase())
+          )
+          .map((item) => ({
+            ...item,
+            categoryId: category.id,
+          }))
+      );
+      setSearchResults(results);
+    };
+
+    useEffect(() => {
+      handleSearch();
+    }, [value]);
+
+    const handleResultClick = (categoryId: string, itemId: string) => {
+      navigate(`/category/${categoryId}/item/${itemId}`);
+    };
+
     return (
       <div className={`${className || "w-full"}`}>
         <div className={"relative flex items-center w-full"}>
-          <div className=" absolute left-3 ">
-            <Search className="h-5 w-5" />
+          <div className="absolute left-3">
+            <Search className="w-5 h-5" />
           </div>
           <Input
             {...props}
@@ -44,7 +100,10 @@ const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
                 ? "focus:border focus:border-error-40"
                 : "focus:border focus:border-primary-40"
             }`}
-            placeholder={props.placeholder || "Searchbar"}
+            placeholder={
+              props.placeholder ||
+              "ex. 명함, 수첩 등 분리 수거 품목을 입력해주세요"
+            }
           />
           {value && (
             <button
@@ -58,6 +117,19 @@ const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
             </button>
           )}
         </div>
+        {searchResults.length > 0 && (
+          <div className="absolute bg-white border shadow-lg rounded-4 w-[23rem] z-10">
+            {searchResults.map((result) => (
+              <div
+                key={result.id}
+                className="p-2 cursor-pointer hover:bg-gray-200"
+                onClick={() => handleResultClick(result.categoryId, result.id)}
+              >
+                {result.name}
+              </div>
+            ))}
+          </div>
+        )}
         {error && (
           <div className="w-full rounded-4 bg-error-5 text-error-30 text-[13px] mt-1 p-2">
             <div className="flex items-center gap-3">
