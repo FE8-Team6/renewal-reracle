448f1e673f86669bc59cd951efb7eeff6b9e4d21
wook <yho7955@naver.com>
2024-11-06 10:30:05 +0900

    refactor: pwa 이름 변경

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
-        name: "Vite PWA Project",
-        short_name: "Vite PWA Project",
+        name: "REracle",
+        short_name: "REracle",
         theme_color: "#ffffff",
         icons: [
           {
