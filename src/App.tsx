41b58b5eaacbcf78d4e30af30adf6983014d0bd1
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-11-07 20:14:13 +0900

    refactor: 주석 제거

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "@shoelace-style/shoelace/dist/themes/light.css";
 // import Loading from "./pages/Loading";
 import { routes } from "./router/routes";
 import { saveWasteCategories } from "./lib/utils/firestoreService";
-// import NotificationWebApi from "./components/NotificationWebApi";
+import NotificationWebApi from "./components/NotificationWebApi";
 
 const router = createBrowserRouter(routes);
 
@@ -17,7 +17,7 @@ const App = () => {
     <>
       <div className="absolute top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 flex flex-col w-full max-w-[440px] h-full max-h-[920px] bg-white">
         <RouterProvider router={router} />
-        {/* <NotificationWebApi /> */}
+        <NotificationWebApi />
       </div>
       {/* <StyledAppContainer> */}
       {/* {isLoading ? <Loading /> : <RouterProvider router={router} />} */}
