86cbae007d0bb3eddc2caed32dc64d2b442edc81
wook <yho7955@naver.com>
2024-11-10 17:48:49 +0900

    refactor: mypage 페이지 레이아웃 모바일 해상도를 고려하게 수정

--- src/components/MyPage/MyPage.tsx
+++ src/components/MyPage/MyPage.tsx
@@ -5,6 +5,7 @@ import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { X } from 'lucide-react';
 import { RecentSearchHistory } from '@/lib/types/search';
 import { deleteSearchHistory, getRecentSearchHistory } from '@/api/searchssApi/recentSearch';
+import KakaoAdfit320x100 from '../KakaoAdfit320x100';
 
 const MyPage = () => {
   const [user, setUser] = useState<{ displayName: string; email: string }>({
@@ -22,7 +23,6 @@ const MyPage = () => {
       fetchRecentSearchHistory(storedUser.uid);
     }
   }, []);
-
   const fetchRecentSearchHistory = async (uid: string) => {
     const history = await getRecentSearchHistory(uid);
     setRecentSearchHistory(history);
@@ -42,79 +42,83 @@ const MyPage = () => {
   };
 
   return (
-    <div className="w-full h-[75vh] p-4 space-y-4 overflow-y-auto">
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-lg text-purple">프로필</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex items-center space-x-4">
-            <img src="/REracle.svg" alt="REracle 대표 아이콘" className="w-12 h-12" />
-            <div className="flex flex-col">
-              <span>{user.displayName}</span>
-              <span className="text-sm text-gray-500">{user.email}</span>
-            </div>
-          </div>
-          <div className="mt-4">
-            <button>
-              <span className="text-sm text-gray-500 ">닉네임 변경</span>
-            </button>
-          </div>
-        </CardContent>
-      </Card>
-
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-lg text-purple">나의 최근 재활용품 검색 리스트</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-wrap gap-2">
-            {recentSearchHistory.map((historyItem) => (
-              <div key={historyItem.id} className="relative px-3 py-1 rounded-full bg-yellow group">
-                <span
-                  className="cursor-pointer text-purple hover:text-purpleDark"
-                  onClick={() => handleNavClick(historyItem.categoryId, historyItem.itemId)}>
-                  #{historyItem.query}
-                </span>
-                <button
-                  onClick={() => handleDeleteClick(historyItem.id)}
-                  className="absolute flex items-center justify-center w-4 h-4 text-white transition-opacity rounded-full -top-1 -right-1 bg-purple">
-                  <X size={12} />
-                </button>
+    <div className="flex flex-col h-[calc(100vh-7rem)]">
+      <KakaoAdfit320x100 />
+      <section className="flex-grow overflow-y-auto w-[22rem] mx-auto pb-4 mt-2">
+        <Card className="mb-4">
+          <CardHeader>
+            <CardTitle className="text-lg text-purple">프로필</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center space-x-4">
+              <img src="/REracle.svg" alt="REracle 대표 아이콘" className="w-12 h-12" />
+              <div className="flex flex-col">
+                <span>{user.displayName}</span>
+                <span className="text-sm text-gray-500">{user.email}</span>
               </div>
-            ))}
-          </div>
-        </CardContent>
-      </Card>
+            </div>
+            <div className="space-y-2">
+              <button>
+                <span className="text-sm text-gray-500">닉네임 변경</span>
+              </button>
+            </div>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-lg text-purple">기타</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="space-y-2">
-            <button>
-              <span className="text-sm text-gray-500 ">REracle 개발 과정</span>
-            </button>
-          </div>
-        </CardContent>
-      </Card>
+        <Card className="mb-4">
+          <CardHeader>
+            <CardTitle className="text-lg text-purple">나의 최근 재활용품 검색 목록</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-wrap gap-2">
+              {recentSearchHistory.map((historyItem) => (
+                <div key={historyItem.id} className="relative px-3 py-1 rounded-full bg-yellow group">
+                  <span
+                    className="cursor-pointer text-purple hover:text-purpleDark"
+                    onClick={() => handleNavClick(historyItem.categoryId, historyItem.itemId)}>
+                    #{historyItem.query}
+                  </span>
+                  <button
+                    onClick={() => handleDeleteClick(historyItem.id)}
+                    className="absolute flex items-center justify-center w-4 h-4 text-white transition-opacity rounded-full -top-1 -right-1 bg-purple">
+                    <X size={12} />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
 
-      <div className="space-y-2 text-center">
-        <Button variant="link" onClick={() => navigate('/myquestion')} className="text-sm">
-          나의 R지식in 보러가기
-        </Button>
-      </div>
+        <Card className="mb-4">
+          <CardHeader>
+            <CardTitle className="text-lg text-purple">기타</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-2">
+              <button>
+                <span className="text-sm text-gray-500">REracle 개발 과정</span>
+              </button>
+            </div>
+          </CardContent>
+        </Card>
 
-      <Button
-        variant="secondary"
-        className="w-full text-white bg-blue-400 hover:bg-blue-500"
-        onClick={() => {
-          localStorage.removeItem('userData');
-          navigate('/login');
-        }}>
-        로그아웃
-      </Button>
+        <div className="space-y-2 text-center">
+          <Button variant="link" onClick={() => navigate('/myquestion')} className="text-sm">
+            나의 R지식in 보러가기
+          </Button>
+        </div>
+        <div className="space-y-2 text-center">
+          <Button
+            variant="secondary"
+            className="w-full text-white bg-blue-400 hover:bg-blue-500"
+            onClick={() => {
+              localStorage.removeItem('userData');
+              navigate('/login');
+            }}>
+            로그아웃
+          </Button>
+        </div>
+      </section>
     </div>
   );
 };
