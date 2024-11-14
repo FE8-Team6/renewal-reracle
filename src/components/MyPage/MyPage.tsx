80ee2e37834bd2124e7d6b11c5fc7fbd85aeb523
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-11-14 09:47:55 +0900

    refactor: mypage 시맨틱 태그 추가

--- src/components/MyPage/MyPage.tsx
+++ src/components/MyPage/MyPage.tsx
@@ -61,69 +61,75 @@ const MyPage = () => {
   return (
     <div className="flex flex-col h-[calc(100vh-9rem)]">
       <KakaoAdfit320x50 />
-      <section className="flex-grow w-full h-full pb-4 mx-auto mt-2 overflow-y-auto">
+      <main className="flex-grow w-full h-full pb-4 mx-auto mt-2 overflow-y-auto">
         <div className="flex flex-col items-center space-y-4">
-          <Card className={`${cardWidth} mb-4`}>
-            <CardHeader>
-              <CardTitle className="text-lg text-purple">프로필</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="flex items-center space-x-4">
-                <img src="/REracle.svg" alt="REracle 대표 아이콘" className="w-12 h-12" />
-                <div className="flex flex-col">
-                  <span>{user.displayName}</span>
-                  <span className="text-sm text-gray-500">{user.email}</span>
-                </div>
-              </div>
-              <div className="mt-4">
-                <button className="text-gray-500 hover:underline">
-                  <span className="text-sm font-medium text-gray-500">닉네임 변경</span>
-                </button>
-              </div>
-              <div className="mt-4">
-                <button onClick={() => navigate('/myquestion')} className="text-sm text-gray-500 hover:underline">
-                  <span className="font-medium text-gray-500">나의 R지식in 보러가기</span>
-                </button>
-              </div>
-            </CardContent>
-          </Card>
-
-          <Card className={`${cardWidth} mb-4`}>
-            <CardHeader>
-              <CardTitle className="text-lg text-purple">나의 최근 재활용품 검색 목록</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="flex flex-wrap gap-2">
-                {recentSearchHistory.map((historyItem) => (
-                  <div key={historyItem.id} className="relative px-3 py-1 rounded-full bg-yellow group">
-                    <span
-                      className="cursor-pointer text-purple hover:text-purpleDark"
-                      onClick={() => handleNavClick(historyItem.categoryId, historyItem.itemId)}>
-                      #{historyItem.query}
-                    </span>
-                    <button
-                      onClick={() => handleDeleteClick(historyItem.id)}
-                      className="absolute flex items-center justify-center w-4 h-4 text-white transition-opacity rounded-full -top-1 -right-1 bg-purple">
-                      <X size={12} />
-                    </button>
+          <section className={`${cardWidth} mb-4`}>
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-lg text-purple">프로필</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="flex items-center space-x-4">
+                  <img src="/REracle.svg" alt="REracle 대표 아이콘" className="w-12 h-12" />
+                  <div className="flex flex-col">
+                    <span>{user.displayName}</span>
+                    <span className="text-sm text-gray-500">{user.email}</span>
                   </div>
-                ))}
-              </div>
-            </CardContent>
-          </Card>
+                </div>
+                <div className="mt-4">
+                  <button className="text-gray-500 hover:underline">
+                    <span className="text-sm font-medium text-gray-500">닉네임 변경</span>
+                  </button>
+                </div>
+                <div className="mt-4">
+                  <button onClick={() => navigate('/myquestion')} className="text-sm text-gray-500 hover:underline">
+                    <span className="font-medium text-gray-500">나의 R지식in 보러가기</span>
+                  </button>
+                </div>
+              </CardContent>
+            </Card>
+          </section>
 
-          <Card className={`${cardWidth} mb-4`}>
-            <CardHeader>
-              <CardTitle className="text-lg text-purple">기타</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="space-y-2">
-                <button className="text-gray-500 hover:underline">
-                  <span className="text-sm font-medium text-gray-500">REracle 개발 과정</span>
-                </button>
-              </div>
-            </CardContent>
-          </Card>
+          <section className={`${cardWidth} mb-4`}>
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-lg text-purple">나의 최근 재활용품 검색 목록</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="flex flex-wrap gap-2">
+                  {recentSearchHistory.map((historyItem) => (
+                    <div key={historyItem.id} className="relative px-3 py-1 rounded-full bg-yellow group">
+                      <span
+                        className="cursor-pointer text-purple hover:text-purpleDark"
+                        onClick={() => handleNavClick(historyItem.categoryId, historyItem.itemId)}>
+                        #{historyItem.query}
+                      </span>
+                      <button
+                        onClick={() => handleDeleteClick(historyItem.id)}
+                        className="absolute flex items-center justify-center w-4 h-4 text-white transition-opacity rounded-full -top-1 -right-1 bg-purple">
+                        <X size={12} />
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </CardContent>
+            </Card>
+          </section>
+
+          <section className={`${cardWidth} mb-4`}>
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-lg text-purple">기타</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="space-y-2">
+                  <button className="text-gray-500 hover:underline">
+                    <span className="text-sm font-medium text-gray-500">REracle 개발 과정</span>
+                  </button>
+                </div>
+              </CardContent>
+            </Card>
+          </section>
 
           <div className={`space-y-2 text-center ${cardWidth}`}>
             <Button
@@ -137,7 +143,7 @@ const MyPage = () => {
             </Button>
           </div>
         </div>
-      </section>
+      </main>
     </div>
   );
 };
