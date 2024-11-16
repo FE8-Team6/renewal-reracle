66d904f50eeff6706d64695b404f8e13931c6293
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-11-16 23:15:05 +0900

    fix: searchbar 에러 수정

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import { cn } from '@/lib/utils';
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      type={type}
-      className={cn(
-        'flex h-full w-[22rem] rounded-5 border border-purple bg-white px-9 py-3 focus:border-purpleDark focus:outline-none',
-        className,
-      )}
-      ref={ref}
-      autoComplete={type === 'password' ? 'current-password' : ''}
-      {...props}
-    />
-  );
-});
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          'flex h-full w-[22rem] rounded-5 border border-purple bg-white px-9 py-3 focus:border-purpleDark focus:outline-none',
+          className,
+        )}
+        ref={ref}
+        autoComplete={type === 'password' ? 'current-password' : ''}
+        {...props}
+      />
+    );
+  },
+);
 Input.displayName = 'Input';
 
 export { Input };
