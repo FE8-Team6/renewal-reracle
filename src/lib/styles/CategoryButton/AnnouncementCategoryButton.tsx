5d149fc9aa211ac18d6aa12c4a1e536aba635f01
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-11-16 23:09:38 +0900

    feat: 공지사항 페이지에서 글의 카테고리를 Firestore에 저장하고 카테고리로 조회할 수 있도록 구현

---
+++ src/lib/styles/CategoryButton/AnnouncementCategoryButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+type AnnouncementCategoryButtonProps = {
+  postCategory: string;
+  isActive: boolean;
+  onClick: () => void;
+};
+
+const AnnouncementCategoryButton: React.FC<AnnouncementCategoryButtonProps> = ({ postCategory, isActive, onClick }) => {
+  return (
+    <Button
+      variant={isActive ? 'default' : 'outline'}
+      size="sm"
+      className={`px-3 py-1 mr-2 mb-2 rounded-full text-sm ${isActive ? 'bg-yellow text-white hover:bg-yellow' : 'bg-gray-200 text-gray-700'}`}
+      onClick={onClick}>
+      {postCategory}
+    </Button>
+  );
+};
+
+export default AnnouncementCategoryButton;
