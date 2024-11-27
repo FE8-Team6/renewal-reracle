36302b4c6f8b59ca4b3678823785655fc46edc41
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-11-27 14:48:11 +0900

    style: 높이 레이아웃 변경

--- src/pages/ReracleGame.tsx
+++ src/pages/ReracleGame.tsx
@@ -160,7 +160,7 @@ const ReraclePuzzle = () => {
   }, [board.length]);
 
   return (
-    <div className="flex flex-col items-center w-full max-w-4xl mx-auto p-4">
+    <div className="flex flex-col items-center w-full min-h-[calc(100vh-2rem)] p-4">
       <ReracleGameTutorialModal isOpen={isTutorialOpen} onClose={() => setIsTutorialOpen(false)} />
 
       <div className="mb-8 text-center">
