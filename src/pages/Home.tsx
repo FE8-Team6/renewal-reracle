ef0eff0e9f509fde45b9b74494a8b9fdf184f7ad
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-11-27 15:28:32 +0900

    style: 퍼즐 게임 style 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,15 @@ export const Home = () => {
   return (
     <main className="flex flex-col min-h-[calc(100vh-8rem)] pb-[5rem] ">
       <Category />
-      <button className="bg-purple text-white p-2 w-1/2 rounded-2" onClick={() => navigate('/game')}>
-        ReraclePuzzle 게임하러가기
-      </button>
+      <div className="my-4 flex items-center justify-center">
+        <button
+          className="bg-purpleLight p-2 w-[40%] rounded-2 flex items-center justify-center gap-2"
+          onClick={() => navigate('/game')}
+        >
+          <img src="/REracle.svg" alt="REracle 아이콘" className="w-9 h-9" />
+          <span>퍼즐 게임</span>
+        </button>
+      </div>
     </main>
   );
 };
