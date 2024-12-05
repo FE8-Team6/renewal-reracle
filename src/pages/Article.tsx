97298c05b4d3827ca748a49f0389e4c4a9c2b945
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-12-05 17:26:06 +0900

    chore: typescript 5.5.3 버전에서 js/ts가 충돌이 발생하여  -> 5.2.3 하위 버전으로 사용하도록 설정

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,5 +1,15 @@
+import { KakaoAdfit320x100, KakaoAdfit320x50 } from '@/components/KakaoAdfit';
+
 const Article = () => {
-  return <div>Article</div>;
+  return (
+    <main className="min-h-[calc(100vh-8rem)] pb-[5rem] ">
+      <KakaoAdfit320x50 />
+      <KakaoAdfit320x100 />
+      <section>
+        <p>article</p>
+      </section>
+    </main>
+  );
 };
 
 export default Article;
