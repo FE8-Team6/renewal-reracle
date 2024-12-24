d00b147d92cf403e8b331aaeea384cb3f2c9c437
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-12-24 14:49:40 +0900

    feat: nav 컴포넌트의 button type 추가 및 aria-label 추가

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -12,31 +12,43 @@ export const Nav = () => {
   };
 
   return (
-    <nav className="w-full max-w-[500px] fixed bottom-0 left-1/2 flex justify-around bg-white -translate-x-1/2  px-1 py-2">
+    <nav
+      className="w-full max-w-[500px] fixed bottom-0 left-1/2 flex justify-around bg-white -translate-x-1/2  px-1 py-2"
+      role="navigation"
+      aria-label="주 네비게이션"
+    >
       <div className="w-full h-[5rem] absolute bottom-0 bg-green flex justify-evenly items-center">
         <button
+          type="button"
           onClick={() => handleNavClick('/')}
+          aria-label="홈으로 이동"
           className="flex flex-col items-center transition-colors duration-300 hover:text-gray-400 focus:text-gray-500"
         >
           <IoHomeOutline className="w-6 h-6" />
           <span className="mt-1 text-sm font-medium text-center text-grayDark">홈</span>
         </button>
         <button
+          type="button"
           onClick={() => handleNavClick('/qna')}
+          aria-label="R지식in으로 이동"
           className="flex flex-col items-center transition-colors duration-300 hover:text-gray-400 focus:text-gray-500"
         >
           <PiArticleNyTimesLight className="w-6 h-6" />
           <span className="mt-1 text-sm font-medium text-center text-grayDark">R지식in</span>
         </button>
         <button
+          type="button"
           onClick={() => handleNavClick('/article')}
+          aria-label="아티클로 이동"
           className="flex flex-col items-center transition-colors duration-300 hover:text-gray-400 focus:text-gray-500"
         >
           <MdOutlineArticle className="w-6 h-6" />
           <span className="mt-1 text-sm font-medium text-center text-grayDark">아티클</span>
         </button>
         <button
+          type="button"
           onClick={() => handleNavClick('/announcement')}
+          aria-label="공지로 이동"
           className="flex flex-col items-center transition-colors duration-300 hover:text-gray-400 focus:text-gray-500"
         >
           <LuClipboardList className="w-6 h-6" />
