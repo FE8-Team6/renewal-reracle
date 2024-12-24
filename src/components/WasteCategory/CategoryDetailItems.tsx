da53f92d6de141d176dd9d65c7c6e6550ea6b95b
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-12-24 15:10:57 +0900

    feat: aria-label 속성 추가 및 tabindex 추가

--- src/components/WasteCategory/CategoryDetailItems.tsx
+++ src/components/WasteCategory/CategoryDetailItems.tsx
@@ -42,37 +42,38 @@ export const CategoryDetailItems = () => {
 
   return (
     <article className="flex flex-col min-h-[calc(100vh-8rem)] pb-[5rem]" aria-labelledby="item-title">
-      <div>
-        <KakaoAdfit320x50 />
-        <div className="flex flex-col items-center px-4">
-          <header className="w-full mt-4">
-            <h1 id="item-title" className="text-2xl font-bold text-purple">
-              {itemsDetails?.name}
-            </h1>
-          </header>
-          {itemsDetails?.imageURL && (
-            <figure
-              className="flex items-center justify-center w-full h-48 my-4 rounded-lg bg-purpleLight"
-              aria-label={`${itemsDetails.name} 이미지`}
-            >
-              <img
-                src={itemsDetails.imageURL}
-                alt={`${itemsDetails.name} 아이콘`}
-                className="max-w-[60%] max-h-[80%] object-contain"
-              />
-            </figure>
-          )}
-          <section className="w-full mt-4" aria-label="배출방법">
-            <h2 className="text-xl font-bold text-purple">배출방법</h2>
-            <ul className="w-full">
-              {itemsDetails?.recyclingInstructions.map((instruction, index) => (
-                <li key={index} className="my-2 font-semibold select-text">
-                  {instruction}
-                </li>
-              ))}
-            </ul>
-          </section>
-        </div>
+      <KakaoAdfit320x50 />
+      <div className="flex flex-col items-center px-4">
+        <header className="w-full mt-4">
+          <h1 id="item-title" className="text-2xl font-bold text-purple" tabIndex={0}>
+            {itemsDetails?.name}
+          </h1>
+        </header>
+        {itemsDetails?.imageURL && (
+          <figure
+            className="flex items-center justify-center w-full h-48 my-4 rounded-lg bg-purpleLight"
+            aria-label={`${itemsDetails.name} 아이콘`}
+            tabIndex={0}
+          >
+            <img
+              src={itemsDetails.imageURL}
+              alt={`${itemsDetails.name} 아이콘`}
+              className="max-w-[60%] max-h-[80%] object-contain"
+            />
+          </figure>
+        )}
+        <section className="w-full mt-4" aria-labelledby="recycling-instructions">
+          <h2 id="recycling-instructions" className="text-xl font-bold text-purple" tabIndex={0}>
+            배출방법
+          </h2>
+          <ul className="w-full">
+            {itemsDetails?.recyclingInstructions.map((instruction, index) => (
+              <li key={index} className="my-2 font-semibold select-text" tabIndex={0}>
+                {instruction}
+              </li>
+            ))}
+          </ul>
+        </section>
       </div>
     </article>
   );
