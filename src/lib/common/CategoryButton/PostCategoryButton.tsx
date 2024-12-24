cfd8e938f658a8ac5e253f24efebe1de6153e8e0
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-12-24 21:48:37 +0900

    feat: QnaPage 카테고리 버튼에 접근성 속성 추가

--- src/lib/common/CategoryButton/PostCategoryButton.tsx
+++ src/lib/common/CategoryButton/PostCategoryButton.tsx
@@ -14,6 +14,9 @@ export const PostCategoryButton: React.FC<CategoryButtonProps> = ({ postCategory
       size="sm"
       className={`px-3 py-1 mr-2 mb-2 rounded-full text-sm ${isActive ? 'bg-purple text-white' : 'bg-gray-200 text-gray-700'}`}
       onClick={onClick}
+      aria-checked={isActive}
+      tabIndex={0}
+      aria-label={`${postCategory} 카테고리 ${isActive ? '선택 되었습니다.' : '선택 하시겠습니까?'}`}
     >
       {postCategory}
     </Button>
