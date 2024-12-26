81b7ee4721bddaad5a449e09646ea7b0fc75e6b6
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-12-26 14:30:49 +0900

    feat: chunkArray함수 테스트 코드 추가

---
+++ src/utils/__test__/chunkArray.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+import { chunkArray } from '../chunkArray';
+
+describe('chunkArray', () => {
+  it('배열을 지정된 크기로 나눕니다', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    const result = chunkArray(array, 2);
+    expect(result).toEqual([
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ]);
+  });
+
+  it('배열의 길이가 chunkSize로 나누어떨어지지 않아도 처리할 수 있습니다', () => {
+    const array = [1, 2, 3, 4, 5];
+    const result = chunkArray(array, 2);
+    expect(result).toEqual([[1, 2], [3, 4], [5]]);
+  });
+
+  it('빈 배열일 경우는 빈 배열을 반환합니다.', () => {
+    const array: number[] = [];
+    const result = chunkArray(array, 2);
+    expect(result).toEqual([]);
+  });
+});
